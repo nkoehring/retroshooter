9e437530c5252c5ce43bc6124831b19e04bf6e4a
Norman Köhring <me@nkoehring.de>
2012-06-05 18:16:26 +0200

    can haz bullets nao! but no hits, yet

--- retroshooter.js
+++ retroshooter.js
@@ -20,8 +20,8 @@ function Ship(x, y, name, colour, multiplicator) {
 }
 
 
-function draw_enemy(ship) {
-  var baseSize = 20 * ship.multiplicator
+function draw_enemy(ship, baseSize) {
+  var baseSize = baseSize * ship.multiplicator
 
   ctx.globalAlpha = ship.hitpoints*1.0 / ship.baseHitpoints
   ctx.fillStyle = ship.colour
@@ -29,13 +29,20 @@ function draw_enemy(ship) {
   ctx.moveTo(ship.x, ship.y)
   ctx.lineTo(ship.x + baseSize, ship.y)
   ctx.lineTo(ship.x + baseSize/2, ship.y + baseSize)
-  ctx.lineTo(ship.x, ship.y)
+  ctx.closePath()
   ctx.fill()
 }
 
-function draw_player(ship) {
-  var baseSize = 15
+function draw_projectile(projectile, baseSize) {
+  ctx.globalAlpha = 1.0
+  ctx.fillStyle = "#FF0"
+  ctx.beginPath()
+  ctx.arc(projectile.x, projectile.y, baseSize, 0, Math.PI*2, true)
+  ctx.closePath()
+  ctx.fill()
+}
 
+function draw_player(ship, baseSize) {
   ctx.globalAlpha = 1.0
   ctx.fillStyle = "#0F0"
   ctx.beginPath();  
@@ -45,18 +52,35 @@ function draw_player(ship) {
     Math.PI*0.5+0.8, Math.PI*0.5-0.8,
     false
   );  
+  ctx.closePath()
   ctx.fill();  
 }
 
 function run(ctx) {
   var enemies = [],
+      projectiles = [],
       player = new Ship(200, 470, "player"),
       lvl = 0,
       lvlIndicator = document.getElementById('level'),
-      enemyIndicator = document.getElementById('enemies')
+      enemyIndicator = document.getElementById('enemies'),
+      projectileIndicator = document.getElementById('projectiles'),
+      projectileBaseSize = 5,
+      playerBaseSize = 15,
+      enemyBaseSize = 20
 
-  function obsolet(element, index, array) {
+  function obsolet_ship(element, index, array) {
     return (element.y <= 480);
+  }
+
+  function obsolet_projectile(element, index, array) {
+    return (element.y >= 0);
+  }
+
+  function projectile(owner) {
+    var x = player.x,
+        y = player.y
+
+    projectiles.push({owner: owner, x: x, y: y})
   }
 
   function generateEnemies() {
@@ -65,9 +89,6 @@ function run(ctx) {
 
     if (r > 0.1) enemies.push(new Ship(x, -20))
     else enemies.push(new Ship(x, -50, "boss", "#F0A", 3))
-
-    // cleanup lost enemies
-    enemies = enemies.filter(obsolet)
   }
 
   function doKeyDown(evt){
@@ -75,19 +96,32 @@ function run(ctx) {
     if (evt.keyCode == 38) player.y -= 5  // up
     if (evt.keyCode == 39) player.x += 5  // right
     if (evt.keyCode == 40) player.y += 5  // down
-    //if (evt.keyCode == 32) //TODO: shoot!
+    if (evt.keyCode == 32) projectile("player")   //TODO: shoot!
+  }
+
+  function cleanup() {
+    // cleanup lost enemies
+    enemies = enemies.filter(obsolet_ship)
+    projectiles = projectiles.filter(obsolet_projectile)
   }
 
   function render() {
     lvlIndicator.innerHTML = lvl
     enemyIndicator.innerHTML = enemies.length
+    projectileIndicator.innerHTML = projectiles.length
 
     ctx.clearRect(0,0,640,480)
     for(i in enemies) {
+      draw_enemy(enemies[i], enemyBaseSize)
       enemies[i].y += 2.0 / enemies[i].multiplicator
-      draw_enemy(enemies[i])
     }
-    draw_player(player)
+    draw_player(player, playerBaseSize)
+    for(i in projectiles) {
+      draw_projectile(projectiles[i], projectileBaseSize)
+      projectiles[i].y -= 4.0
+    }
+
+    cleanup()
   }
 
   function raiseLevel() { lvl++ }
